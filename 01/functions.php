fc4a39f049a3a08c268a2e4056b165d39f67ad12
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-05 00:56:21 +0300

    existsUser($login) is added

--- 01/functions.php
+++ 01/functions.php
@@ -5,3 +5,9 @@ function getUsersList()
     $users = include __DIR__ . '/users.php';
     return $users;
 }
+
+function existsUser($login): bool
+{
+    $users = getUsersList();
+    return isset($users[$login]);
+}
