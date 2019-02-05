d651a247a56b57f5e3d7741ca7d6867aa42728b7
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-05 23:34:59 +0300

    write to log is implemented

--- 02/saveImage.php
+++ 02/saveImage.php
@@ -23,6 +23,10 @@ if (isset($_FILES['image'])) {
         $isImage = strpos($imageMimeType, 'image') === 0;
         if ($isImage) {
             $isSuccess = move_uploaded_file($savedImagePath, __DIR__ . '/img/' . $imageName);
+
+            $date = date(DATE_ATOM);
+            $logString = [$date, $userName, 'save image', $imageName];
+            file_put_contents(__DIR__ . '/img/log.txt', implode(' | ', $logString) . PHP_EOL, FILE_APPEND);
         }
     }
 }
@@ -32,6 +36,6 @@ if ($isSuccess) {
     exit;
 } elseif (!$isImage) {
     ?>Загруженный файл не является изображением<?php
-}else{
-    ?>Не удалось сохранить файл <?php echo $imageName;?>на сервере<?php
+} else {
+    ?>Не удалось сохранить файл <?php echo $imageName; ?>на сервере<?php
 }
